66e41f9e3fbc06461706fac2791bb4d7cec04eb1
suy <suyan@confirmationcall.com.br>
2025-05-21 07:54:36 -0300

    Iniciado projeto.

---
+++ src/main/java/com/beautyhub/beautyhubelk/BeautyHubElkApplication.java
@@ -0,0 +1,13 @@
+package com.beautyhub.beautyhubelk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautyHubElkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeautyHubElkApplication.class, args);
+    }
+
+}
